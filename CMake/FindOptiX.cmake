0fbce37bec21370c34dca3128ec2a3f5db7a7888
Joao Barbosa <jbarbosa@tacc.utexas.edu>
2014-09-19 15:55:52 -0500

    Minor change to FindOptix

--- CMake/FindOptiX.cmake
+++ CMake/FindOptiX.cmake
@@ -133,8 +133,8 @@ function(OptiX_add_imported_library name lib_location dll_lib dependent_libs)
 endfunction()
 
 # Sets up a dummy target
-OptiX_add_imported_library(optix "${optix_LIBRARY}" "${optix_DLL}" "${CUDA_CUDART_LIBRARY};${OPENGL_LIBRARIES}")
-OptiX_add_imported_library(optixu   "${optixu_LIBRARY}"   "${optixu_DLL}"   "")
+#OptiX_add_imported_library(optix "${optix_LIBRARY}" "${optix_DLL}" "${CUDA_CUDART_LIBRARY};${OPENGL_LIBRARIES}")
+#OptiX_add_imported_library(optixu   "${optixu_LIBRARY}"   "${optixu_DLL}"   "")
 
 # Since liboptix.1.dylib is built with an install name of @rpath, we need to
 # compile our samples with the rpath set to where optix exists.
