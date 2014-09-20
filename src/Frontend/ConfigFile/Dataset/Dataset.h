246cb702cb8b49c305964ffa2846dba8e693df7e
Randall Smith <agrippa@tacc.utexas.edu>
2014-09-20 00:07:51 -0500

    adding config changes for Optix

--- src/Frontend/ConfigFile/Dataset/Dataset.h
+++ src/Frontend/ConfigFile/Dataset/Dataset.h
@@ -5,59 +5,57 @@
 #ifndef GVT_CF_DATASET_H
 #define GVT_CF_DATASET_H
 
-
-#include <GVT/Data/primitives.h>
-#include <GVT/Domain/domains.h>
-#include <Backend/Manta/Domain/MantaDomain.h>
 #include <cfloat>
 #include <map>
 #include <string>
 #include <vector>
 
-#include <GVT/common/debug.h>
+#include <Backend/Manta/Domain/MantaDomain.h>
+#include <Backend/Optix/Domain/OptixDomain.h>
+#include <GVT/Data/primitives.h>
 #include <GVT/DataSet/Dataset.h>
+#include <GVT/Domain/domains.h>
+#include <GVT/common/debug.h>
+
 using namespace std;
 
 namespace GVT {
-    namespace Dataset {
+namespace Dataset {
 
-        template<typename DomainType>
-        class Dataset : public GVTDataset {
-        public:
+template <typename DomainType>
+class Dataset : public GVTDataset {
+ public:
+  Dataset() {}
 
-            Dataset() {
-            }
+  Dataset(string& filename) : GVTDataset(), conf_filename(filename) {
+    GVT_DEBUG(DBG_ALWAYS, "Filename : " + filename);
+    conf_filename = filename;
+  }
 
-            Dataset(string& filename) : GVTDataset(), conf_filename(filename) {
-                GVT_DEBUG(DBG_ALWAYS, "Filename : " + filename);
-                conf_filename = filename;
-            }
+  virtual bool init() {
+    GVT_DEBUG(DBG_ALWAYS, "Generic load");
+    return false;
+  }
 
-            virtual bool init() {
-                GVT_DEBUG(DBG_ALWAYS,"Generic load");
-                return false;
-            }
-            
-        private:
-            vector<string> files;
-//            map<int, DomainType> dom_cache; // XXX TODO fix when this made subclass
-//            map<int, GVT::Data::box3D > dom_bbox;
-//            map<int, GVT::Math::AffineTransformMatrix<float> > dom_model;
-            string conf_filename;
-
-
-        };
-
-        template<> bool Dataset<GVT::Domain::VolumeDomain>::init();
-//        template<> GVT::Domain::Domain* Dataset<GVT::Domain::VolumeDomain>::getDomain(int id);
-        
-        template<> bool Dataset<GVT::Domain::GeometryDomain>::init();
-//        template<> GVT::Domain::Domain* Dataset<GVT::Domain::GeometryDomain>::getDomain(int id);
-
-        template<> bool Dataset<GVT::Domain::MantaDomain>::init();
-//        template<> GVT::Domain::Domain* Dataset<GVT::Domain::MantaDomain>::getDomain(int id);
-        
-    };
+ private:
+  vector<string> files;
+  string conf_filename;
 };
 
-#endif // GVT_DATASET_H
+template <>
+bool Dataset<GVT::Domain::VolumeDomain>::init();
+
+template <>
+bool Dataset<GVT::Domain::GeometryDomain>::init();
+
+template <>
+bool Dataset<GVT::Domain::MantaDomain>::init();
+
+template <>
+bool Dataset<GVT::Domain::OptixDomain>::init();
+
+}  // namespace Dataset
+
+}  // namespave GVT
+
+#endif  // GVT_DATASET_H
--- src/GVT/DataSet/Dataset.cpp
+++ src/GVT/DataSet/Dataset.cpp
@@ -4,55 +4,54 @@
 #include <boost/range/algorithm.hpp>
 #include <boost/foreach.hpp>
 namespace GVT {
-    namespace Dataset {
+namespace Dataset {
 
-        bool GVTDataset::init() {
-            GVT_DEBUG(DBG_ALWAYS, "Abstract load");
-            return false;
-        };
+bool GVTDataset::init() {
+  GVT_DEBUG(DBG_ALWAYS, "Abstract load");
+  return false;
+};
 
-        bool GVTDataset::intersect(GVT::Data::ray&  r, GVT::Data::isecDomList& inter) {
-            if (dataSetBB.intersect(r) || dataSetBB.inBox(r)) {
-                r.t = FLT_MAX;
-                BOOST_FOREACH(GVT::Domain::Domain* d, domainSet) d->intersect(r, inter);
-                boost::sort(inter);
-                
-                
-//                GVT::Data::isecDomList r; r.assign(inter.rbegin(),inter.rend());
-//                inter.clear(); inter.assign(r.begin(),r.end());
-                return (!inter.empty());
-            }
-            return false;
-        };
+bool GVTDataset::intersect(GVT::Data::ray& r, GVT::Data::isecDomList& inter) {
+  if (dataSetBB.intersect(r) || dataSetBB.inBox(r)) {
+    r.t = FLT_MAX;
+    BOOST_FOREACH(GVT::Domain::Domain * d, domainSet) d->intersect(r, inter);
+    boost::sort(inter);
 
-        GVT::Domain::Domain* GVTDataset::getDomain(int id) {
-            GVT_ASSERT_BACKTRACE(id < domainSet.size(),"Getting domain outside bound");
-            return domainSet[id];
-        }
+    //                GVT::Data::isecDomList r;
+    // r.assign(inter.rbegin(),inter.rend());
+    //                inter.clear(); inter.assign(r.begin(),r.end());
+    return (!inter.empty());
+  }
+  return false;
+};
 
-        GVT::Data::LightSource* GVTDataset::getLightSource(int id) {
-            GVT_ASSERT_BACKTRACE(id <lightSet.size(),"Getting light source outside bound");
-            return lightSet[id];
-        }
+GVT::Domain::Domain* GVTDataset::getDomain(int id) {
+  GVT_ASSERT_BACKTRACE(id < domainSet.size(), "Getting domain outside bound");
+  return domainSet[id];
+}
 
-        int GVTDataset::addDomain(GVT::Domain::Domain* dom) {
-            dataSetBB.merge(dom->getWorldBoundingBox());
-            domainSet.push_back(dom);
-            dom->setDomainID(domainSet.size() - 1);
-            
-            GVT_DEBUG(DBG_ALWAYS,"Add domain. World BB : " << dataSetBB);
-            
-            return domainSet.size() - 1;
-        }
+GVT::Data::LightSource* GVTDataset::getLightSource(int id) {
+  GVT_ASSERT_BACKTRACE(id < lightSet.size(),
+                       "Getting light source outside bound");
+  return lightSet[id];
+}
 
-        int GVTDataset::addLightSource(GVT::Data::LightSource* ls) {
-            dataSetBB.merge(ls->getWorldBoundingBox());
-            lightSet.push_back(ls);
-            return domainSet.size() - 1;
-        }
-        
-        int GVTDataset::size() {
-            return domainSet.size();
-        }
-    }
+int GVTDataset::addDomain(GVT::Domain::Domain* dom) {
+  dataSetBB.merge(dom->getWorldBoundingBox());
+  domainSet.push_back(dom);
+  dom->setDomainID(domainSet.size() - 1);
+
+  GVT_DEBUG(DBG_ALWAYS, "Add domain. World BB : " << dataSetBB);
+
+  return domainSet.size() - 1;
+}
+
+int GVTDataset::addLightSource(GVT::Data::LightSource* ls) {
+  dataSetBB.merge(ls->getWorldBoundingBox());
+  lightSet.push_back(ls);
+  return domainSet.size() - 1;
+}
+
+int GVTDataset::size() { return domainSet.size(); }
+}
 }
