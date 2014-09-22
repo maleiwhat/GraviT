5aa9cc76b3e50940b323b2aa5ac32bddc3f5d1a6
Randall Smith <agrippa@tacc.utexas.edu>
2014-09-21 22:40:29 -0500

    working on making it work

--- CMake/boost.cmake
+++ CMake/boost.cmake
@@ -3,7 +3,7 @@ set(Boost_USE_MULTITHREADED      ON)
 set(Boost_USE_STATIC_RUNTIME     ON)
 
 
-find_package(Boost 1.56.0 COMPONENTS system thread timer chrono)
+find_package(Boost 1.55.0 COMPONENTS system thread timer chrono)
 if(Boost_FOUND)
     include_directories(${Boost_INCLUDE_DIRS})
 endif()
