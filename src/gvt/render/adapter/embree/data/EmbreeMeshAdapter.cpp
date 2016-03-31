599dcd4ba80359123d309dd60c7031881d448516
jbarbosa@tacc.utexas.edu <jbarbosa@c221-302.maverick.tacc.utexas.edu>
2016-03-30 20:49:44 -0500

    Fixing adapater condition

--- src/gvt/render/adapter/embree/data/EmbreeMeshAdapter.cpp
+++ src/gvt/render/adapter/embree/data/EmbreeMeshAdapter.cpp
@@ -622,7 +622,7 @@ struct embreeParallelTrace {
               }
             } else {
               valid[pi] = 0;
-              if (!(*adapter).unique || r.type == gvt::render::actor::Ray::SHADOW) localDispatch.push_back(r);
+              if (!(*adapter).unique || ((*adapter).unique && r.type == gvt::render::actor::Ray::SHADOW)) localDispatch.push_back(r);
               // ray is valid, but did not hit anything, so add to dispatch
               // queue and disable it
             }
