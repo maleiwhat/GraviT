055395ae8e80ffc08c73c9ea5273863db441082d
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2016-07-07 14:07:58 -0600

    add timer to intersect

--- src/gvt/render/actor/RayPacket.h
+++ src/gvt/render/actor/RayPacket.h
@@ -75,7 +75,7 @@ template <size_t simd_width> struct RayPacketIntersection {
       t[i] = ray.t_max;
       mask[i] = 1;
     }
-#pragma vector aligned 
+#pragma vector aligned
     for (; i < simd_width; ++i) {
       t[i] = -1;
       mask[i] = -1;
@@ -83,6 +83,10 @@ template <size_t simd_width> struct RayPacketIntersection {
   }
 
   inline bool intersect(const gvt::render::data::primitives::Box3D &bb, int hit[], bool update = false) {
+    &ray_begin, const RayVector::iterator &ray_end) {
+ #ifdef __USE_TAU
+     TAU_PROFILE("RayPacket.h::intersect","",TAU_DEFAULT);
+ #endif
     float lx[simd_width];
     float ly[simd_width];
     float lz[simd_width];
