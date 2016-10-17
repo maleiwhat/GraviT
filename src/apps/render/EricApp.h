0ad639c6575e2d90d1c283d1aebac6ba63b82299
Eric Lee <ericlee123@gmail.com>
2016-10-17 14:15:20 -0500

    setting up my own mpi app; running sbatch results in seg fault

---
+++ src/apps/render/EricApp.h
@@ -0,0 +1,54 @@
+#ifndef APPS_RENDER_ERIC_APP_H
+#define APPS_RENDER_ERIC_APP_H
+
+#include <string>
+#include <glm/glm.hpp>
+
+namespace apps {
+namespace render {
+namespace mpi {
+namespace commandline {
+
+struct Options {
+  enum TracerType {
+    ASYNC_DOMAIN = 0,
+    ASYNC_IMAGE,
+    SYNC_DOMAIN,
+    SYNC_IMAGE,
+    PING_TEST,
+    NUM_TRACERS
+  };
+
+  enum AdapterType { EMBREE, MANTA, OPTIX };
+
+  int tracer;
+  int adapter;
+  int width;
+  int height;
+  bool obj;
+  int instanceCountX;
+  int instanceCountY;
+  int instanceCountZ;
+  int numFrames;
+  int numTbbThreads;
+  std::string infile;
+  std::string model_name;
+  glm::vec3 light_position;
+  glm::vec3 light_color;
+  glm::vec3 eye;
+  glm::vec3 look;
+  glm::vec3 up;
+  float fov;
+  int ray_depth;
+  int ray_samples;
+  int warmup_frames;
+  int active_frames;
+  bool interactive;
+};
+
+}  // namespace commandline
+}  // namespace mpi
+}  // namespace render
+}  // namespace apps
+
+#endif
